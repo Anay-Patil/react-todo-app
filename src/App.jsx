0a8712bc24aa85f76cf8f7e317791775d8613bfc
Anay Patil <anaypatil0@gmail.com>
2025-02-17 23:43:15 +0530

    Add Static Webpage code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-// import "./index.css"
 
 export default function App() {
   return (
-    <h1>Initial Setup</h1>
+    <>
+    <form className="new-item-form">
+          <div className="form-row">
+            <label htmlFor="item">New Item</label>
+            <input type="text" id="item" />
+          </div>      
+          <button className="btn"> Add </button>
+    </form>
+    <h1 className="header">Todo List</h1>
+    <ul className="list">
+      <li>
+        <label>
+            <input type="checkbox" /> 
+              Item 1
+        </label>
+        <button className="btn btn-danger">Delete </button>
+      </li>
+      <li>
+        <label>
+            <input type="checkbox" /> 
+              Item 2
+        </label>
+        <button className="btn btn-danger">Delete </button>
+      </li>
+    </ul>
+    </>
   )
+  
 }
